fb5ccdc1c941f45dc1da340a2b7c6a2a9e27cae2
Gerd Zellweger <mail@gerdzellweger.com>
2015-07-17 21:57:51 -0800

    Update documentation

--- implementors/core/default/trait.Default.js
+++ implementors/core/default/trait.Default.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['raw_cpuid'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='raw_cpuid/struct.CpuIdResult.html' title='raw_cpuid::CpuIdResult'>CpuIdResult</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='raw_cpuid/struct.CpuIdCacheParameters.html' title='raw_cpuid::CpuIdCacheParameters'>CpuIdCacheParameters</a>",];
+implementors['raw_cpuid'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='raw_cpuid/struct.CpuIdResult.html' title='raw_cpuid::CpuIdResult'>CpuIdResult</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/core/iter/trait.Iterator.js
+++ implementors/core/iter/trait.Iterator.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['raw_cpuid'] = ["impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='raw_cpuid/struct.CacheInfoIter.html' title='raw_cpuid::CacheInfoIter'>CacheInfoIter</a>&lt;'a&gt;",];
+implementors['raw_cpuid'] = ["impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='raw_cpuid/struct.CacheInfoIter.html' title='raw_cpuid::CacheInfoIter'>CacheInfoIter</a>&lt;'a&gt;","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='raw_cpuid/struct.CpuIdCacheParameterIter.html' title='raw_cpuid::CpuIdCacheParameterIter'>CpuIdCacheParameterIter</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='raw_cpuid/struct.CpuIdExtendedTopologyIter.html' title='raw_cpuid::CpuIdExtendedTopologyIter'>CpuIdExtendedTopologyIter</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
